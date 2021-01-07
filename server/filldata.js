dab906321032267d2dbc7d4469c84dd6374d8e7f
ramandeep <rdbhagat999@gmail.com>
2021-01-08 00:33:06 +0530

    adds faker data

---
+++ server/filldata.js
@@ -0,0 +1,18 @@
+const faker = require('faker');
+
+const database = {
+  employees: []
+};
+
+for (let i = 1; i <= 50; i++) {
+  database.employees.push({
+    id: i,
+    name: faker.name.findName(),
+    jobtype: faker.name.jobTitle(),
+    email: faker.internet.email(),
+    address: faker.address.streetAddress(),
+    imageUrl: faker.image.avatar()
+  });
+}
+
+console.log(JSON.stringify(database));
