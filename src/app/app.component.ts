1f201c97ad63760f0c4e89232c8e5187c43018e2
Ramandeep <rdbhagat999@gmail.com>
2021-04-11 22:29:18 +0530

    adds toggleNavbar function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('collapsed', style({
+        height: '0px',
+        opacity: 0.5,
+      })),
+      state('expanded', style({
+        // height: '400px',
+        opacity: 1,
+      })),
+      transition('collapsed => expanded', [
+        animate('500ms ease-in-out')
+      ]),
+      transition('expanded => collapsed', [
+        animate('500ms ease-in')
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'ngtest';
+
+  isCollapsed = true;
+
+  ngOnInit() {
+    this.isCollapsed = true;
+  }
+
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
